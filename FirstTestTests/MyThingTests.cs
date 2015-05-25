c80487a3ab303d48a1a290f859d0d2ae6e7ee699
MalcolmAnderson <malcolm.b.anderson@pragmaticagility.com>
2015-05-25 02:16:11 -0700

    A first commit

---
+++ FirstTestTests/MyThingTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using FirstTest;
+
+namespace FirstTest
+{
+    [TestClass]
+    public class MyThingTests
+    {
+        [TestMethod]
+        public void MyThingShouldInstantiate()
+        {
+            FirstTest.MyThing o = new FirstTest.MyThing();
+            Assert.AreEqual(o.GetType().ToString(), "FirstTest.MyThing"); 
+            //adding a comment to force a synch
+        }
+    }
+}
